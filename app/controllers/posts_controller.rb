644f4e9f443cce3e256aa1a8f1d3973b65536304
GGG <kuranberitorappu@gmail.com>
2022-03-27 08:39:11 +0000

    postsにfile変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class SpeaksController < ApplicationController
+class PostsController < ApplicationController
   before_action :set_content, only:[:edit, :show, :update, :destroy]
   def index
     @post = Post.all
@@ -9,12 +9,12 @@ class SpeaksController < ApplicationController
   end
 
   def create
-    @post = Post.new(speak_param)
+    @post = Post.new(post_param)
     if params[:back]
       render :new
     else
       if @post.save
-        redirect_to speaks_path, notice:"保存しました"
+        redirect_to posts_path, notice:"保存しました"
       else
         render :new
       end
@@ -28,24 +28,24 @@ class SpeaksController < ApplicationController
   end
 
   def update
-    if @post.update(speak_param)
-      redirect_to speak_path, notice:"編集しました"
+    if @post.update(post_param)
+      redirect_to post_path, notice:"編集しました"
     end
   end
 
   def destroy
     @post.destroy
-    redirect_to speak_path, notice:"削除しました"
+    redirect_to post_path, notice:"削除しました"
   end
 
   def confirm
-    @post = Post.new(speak_param)
+    @post = Post.new(post_param)
     render :new if @post.invalid?
   end
 
   private
 
-  def speak_param
+  def post_param
     params.require(:posts).permit(:content)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :speaks do
+  resources :posts do
     collection do
       post :confirm
     end
