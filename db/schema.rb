57ab8a718a7f38e127927f92ee71abe64ad337af
GGG <kuranberitorappu@gmail.com>
2022-03-27 03:42:49 +0000

    ロールバックを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_021635) do
+ActiveRecord::Schema.define(version: 2022_03_25_021126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
-  end
-
-  create_table "speaks", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
